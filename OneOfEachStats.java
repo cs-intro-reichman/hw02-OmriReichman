a185a3a3e7b4d9d5010662e70a5a083ea3f7da13
Omri Pinchas <omripinchas.davidov@post.runi.ac.il>
2024-01-03 22:45:10 +0200

    OneOfEach.

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -14,7 +14,47 @@ public class OneOfEachStats {
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
         Random generator = new Random(seed);  
-		
+		 int T = Integer.parseInt(args[0]);
+        int two = 0;
+        int three = 0;
+        int four = 0;
+        int total = 0;
+       
+        for (int i=0; i<T;i++) {
+            Boolean g = true;
+        Boolean b = true;
+        int n = 0;
+        
+        while ( b || g) {
+           double x = generator.next.Double();
+           if (x>=0.5) {
+             b = false;
+           } if (x<0.5) {
+             g = false;
+           }
+              n++;
+              total++;
+               if ((n==2) && (g==false) && (b==false)) {
+            two++;
+        } else if ((n==3) && (g==false) && (b==false)) {
+            three++;
+        } else if ((n>=4) && (g==false) && (b==false)) {
+            four++;
+        }
+         }
+        }
+        double average = (double) total/T;
+        System.out.println("Average: " + average + " children to get at least one of each gender." );
+        System.out.println("Number of families with 2 children: " + two);
+        System.out.println("Number of families with 3 children: " + three);
+        System.out.println("Number of families with 4 or more children: " + four);
+         if (two>=three && two>=four) {
+            System.out.println("The most common number of children is 2.");
+        } else if (three>two && three>=four) {
+            System.out.println("The most common number of children is 3.");
+        } else {
+            System.out.println("The most common number of children is 4 or more.");
+        }
 		//// In the previous version of this program, you used a statement like:
 		//// double rnd = Math.random();
 		//// Where "rnd" is the variable that stores the generated random value.
